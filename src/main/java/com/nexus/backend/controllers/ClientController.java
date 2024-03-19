5f80fdd37286cca0a6a9f8d0520cde337fc52557
Darlonfranklin <darlonfranklin88@gmail.com>
2024-03-19 17:30:59 -0300

    Implementation of service and controller of Customer Registration, methods CREATE, GET, DELETE, UPDATE

--- src/main/java/com/nexus/backend/controllers/ClientController.java
+++ src/main/java/com/nexus/backend/controllers/ClientController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -22,6 +24,31 @@ public class ClientController {
         Client newObj = service.create(objDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientDTO>> findAll() {
+        List<Client> list = service.findAll();
+        List<ClientDTO> listDTO = list.stream().map(ClientDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        Client obj = service.findById(id);
+        return ResponseEntity.ok().body(new ClientDTO(obj));
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO objDTO) {
+        Client obj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new ClientDTO(obj));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/nexus/backend/services/ClientService.java
+++ src/main/java/com/nexus/backend/services/ClientService.java
@@ -4,8 +4,13 @@ package com.nexus.backend.services;
 import com.nexus.backend.domain.Client;
 import com.nexus.backend.domain.dtos.ClientDTO;
 import com.nexus.backend.repositories.ClientRepository;
+import com.nexus.backend.services.exceptions.ObjectNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -16,5 +21,26 @@ public class ClientService {
     public Client create(ClientDTO objDTO) {
         Client newObj = new Client(objDTO);
         return repository.save(newObj);
+    }
+
+    public List<Client> findAll() {
+        return repository.findAll();
+    }
+
+    public Client findById(Long id) {
+        Optional<Client> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found! " + id));
+    }
+
+    public Client update(Long id, @Valid ClientDTO objDTO) {
+        objDTO.setId(id);
+        Client oldObj = findById(id);
+        oldObj = new Client(objDTO);
+        return repository.save(oldObj);
+    }
+
+    public void delete(Long id) {
+        Client obj = findById(id);
+        repository.deleteById(id);
     }
 }
