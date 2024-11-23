b994848c38e51595a1507148bed183b7b2709b03
DARLON <darlonfranklin88@gmail.com>
2024-11-23 18:37:21 -0300

    Alteration tests

--- src/test/resources/sql/dataClient.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_client (id, name, cpf, sex, phone, email, cep, street_name, complement, neighborhood, number, locality, uf, country)
-VALUES (1, 'John Doe', '987.654.321-00', 'M', '(11) 98765-4321', 'johndoe@example.com', '12345-678', 'Main Street', 'Apt 101', 'Central', '123', 'New York', 'NY', 'USA');
